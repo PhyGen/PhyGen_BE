7ab8468637cc362e2bb7d96380c6014234f88bb3
Tri Nguyen Minh <minhtri10504@gmail.com>
2025-06-10 08:34:43 +0700

    Update Program.cs

--- teamseven.PhyGen.API/Program.cs
+++ teamseven.PhyGen.API/Program.cs
@@ -130,7 +130,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhyGen API V1");
+    c.SwaggerEndpoint("/backend/swagger/v1/swagger.json", "PhyGen API V1");
     c.RoutePrefix = "swagger";
 });
 
