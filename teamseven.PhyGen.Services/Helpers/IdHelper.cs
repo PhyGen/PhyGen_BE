aa4175957ececab0b0f873e8d368ffd9e9592e0f
Nguyen Tran Khanh Kien <nguyenkien264038@gmail.com>
2025-07-05 22:21:41 +0700

    add encode url for grade

---
+++ teamseven.PhyGen.Services/Helpers/IdHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamseven.PhyGen.Services.Helpers
+{
+    public static class IdHelper
+    {
+        public static string EncodeId(int id)
+        {
+            return Convert.ToBase64String(BitConverter.GetBytes(id));
+        }
+
+        public static int DecodeId(string encodedId)
+        {
+            try
+            {
+                var bytes = Convert.FromBase64String(encodedId);
+                return BitConverter.ToInt32(bytes, 0);
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid encoded ID format.");
+            }
+        }
+    }
+}
